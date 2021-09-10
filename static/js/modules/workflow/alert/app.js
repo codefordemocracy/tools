51091666c3d2ab32de6c331b923def31d026baf8
Kathy Qian <hello@kathyqian.com>
2021-09-10 11:20:51 -0700

    creation workflow for alerts

--- static/js/modules/workflow/alert/app.js
+++ static/js/modules/workflow/alert/app.js
@@ -1,9 +1,67 @@
 /* Initialize Elements */
 
 new Vue({
+  store,
+  router,
   el: '#app',
   components: {
     'tabs': tabbed.tabs,
-    'tab': tabbed.tab
+    'tab': tabbed.tab,
+    'querysearcher': querysearcher
   },
+  data: {
+    query: undefined,
+    trigger: {
+      event: 'new_results',
+      frequency: 'weekly'
+    },
+    save: {
+      name: null,
+      description: null
+    }
+  },
+  computed: {
+    build() {
+      let obj = {
+        trigger: this.trigger,
+        name: this.save.name,
+        description: this.save.description
+      }
+      if (!_.isUndefined(this.query)) {
+        obj.query = this.query.selected.id
+      }
+      return obj
+    }
+  },
+  methods: {
+    updateQuery(payload) {
+      this.query = payload
+    },
+    submit() {
+      let endpoint = '/api/alert/create/'
+      if (this.$route.query.action == 'edit') {
+        endpoint = '/api/alert/edit/'
+      }
+      this.$store.dispatch('workflow/submit', {endpoint: endpoint, payload: this.build})
+    }
+  },
+  watch: {
+    build: {
+      deep: true,
+      handler() {
+        store.commit('workflow/reset')
+        store.commit('workflow/clear')
+        // validate
+        if (!_.isUndefined(this.query)) {
+          store.commit('workflow/valid', 1)
+        }
+        if (!_.isEmpty(this.trigger.event) && !_.isEmpty(this.trigger.frequency)) {
+          store.commit('workflow/valid', 2)
+        }
+        if (!_.isEmpty(this.save.name) && !_.isEmpty(this.save.description)) {
+          store.commit('workflow/complete')
+        }
+      }
+    }
+  }
 })
